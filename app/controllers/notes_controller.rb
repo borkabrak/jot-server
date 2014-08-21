22458d5a0c3b5a4444a0984f70ce86ccd5dceac5
Jonathan Carter <borkabrak@gmail.com>
2014-08-21 00:09:51 -0500

    Note can be created, listed, and shown individually.  List and show available as json.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,33 @@
 class NotesController < ApplicationController
   def index
-    @notes = Notes.all
+    @notes = Note.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @notes }
+    end
   end
+
+  def new
+  end
+
+  def show
+    @note = Note.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @note }
+    end
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.save
+    redirect_to notes_path
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:body)
+  end
+
 end
